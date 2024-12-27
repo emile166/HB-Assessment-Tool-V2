6ff1512bf4f5bd1668690a3d1924ff6ac1c9f7f5
emile166 <emilemodesitt@gmail.com>
2024-12-27 10:30:14 -0800

    Updates dashboard rules

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -34,8 +34,8 @@ function Dashboard({ onSelectQuestionnaire }) {
             <h2 className="text-xl font-bold text-red-500 mb-2 text-center underline">Rules:</h2>
             <ol className="list-decimal list-inside text-md">
               <li>Assess one injury at a time.</li>
+              <li>Only select answers that are *relevant to the injury you’re assessing.* (If a test causes completely unrelated symptoms, don't put that in your answers.)</li>
               <li>Start with the primary assessment unless otherwise instructed.</li>
-              <li>Only select answers that are relevant to the injury you’re assessing.</li>
               <li>Complete all applicable assessments in the one sitting (typically 20-40 minutes in total).</li>
               <li>Do not close the tab or refresh the page between assessments.</li>
               <li>Follow instructions carefully. Improper testing will lead to inaccurate results.</li>
