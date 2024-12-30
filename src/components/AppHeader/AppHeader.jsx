455c5d74356e9a53d2980c4d479c7009aebf86d8
emile166 <emilemodesitt@gmail.com>
2024-12-30 12:13:09 -0800

    App header style updates: - Add mountain background image - Reorder text for better visual hierarchy - Make button smaller - Change title text - Add HB logo to top left corner

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -4,34 +4,50 @@ import { Button } from "../ui/button";
 
 const AppHeader = forwardRef((props, ref) => {
   return (
-    <div ref={ref} className="bg-primary/25 text-center p-8 rounded-lg">
-      <a href="https://www.hoopersbeta.com/finger-tool" className="text-2xl text-black font-bold mb-2">
-        Hooper's Beta Finger Injury Assessment Tool
-      </a>
-      <p className="text-gray-500 uppercase">For rock climbing related finger injuries</p>
+    <div
+      ref={ref}
+      className="bg-primary/25 text-center p-8 rounded-lg relative"
+      style={{
+        backgroundImage: 'url("https://images.squarespace-cdn.com/content/5e3ca2f99bfdc338a02cefb9/3070c347-4879-4e84-95d3-68677e1c21e4/mountain+illustration+for+assessment+tool+header+background.jpg?content-type=image%2Fjpeg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
 
-      <p className="text-gray-500 text-xs">By using this tool, you agree to our <a
-        href="https://hoopersbeta.com/termsandconditions"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="underline text-gray-500"
-      >
-        terms and conditions.
-      </a>
-      </p>
+      <div className="absolute top-0 left-0 p-4">
+        <a href="https://www.hoopersbeta.com/">
+          <img src="https://images.squarespace-cdn.com/content/5e3ca2f99bfdc338a02cefb9/65b6f7e8-c3f1-437f-81c5-bd243e7a0a8b/Logo+with+HB+centered.png?content-type=image%2Fpng" alt="Hooper's Beta Logo" className="size-12" />
+        </a>
+      </div>
 
-      <p className="mt-8 text-gray-500">Completed the assessment?</p>
+      <div className="relative z-10">
+        <a href="https://www.hoopersbeta.com/finger-tool" className="text-2xl text-black font-bold mb-2">
+          Free Finger Injury Assessment Tool
+        </a>
+        <p className="text-gray-500 uppercase">For rock climbing related finger injuries</p>
 
-      <Button className="bg-secondary-foreground hover:bg-secondary-foreground/80 w-fit mx-auto min-h-[4rem] md:min-h-[3rem] px-6 py-3">
-        <a
-          href="https://hoopersbeta.com/recoveryblueprint"
+        <Button className="bg-secondary-foreground hover:bg-secondary-foreground/80 w-fit mx-auto min-h-[3rem] md:min-h-[2.5rem] px-4 py-2 mt-8">
+          <a
+            href="https://hoopersbeta.com/recoveryblueprint"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs text-white"
+          >
+            GET AN INJURY-SPECIFIC RECOVERY PROGRAM HERE
+          </a>
+        </Button>
+
+        <p className="text-gray-500 text-xs mt-2">By using this tool, you agree to our <a
+          href="https://hoopersbeta.com/termsandconditions"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-sm text-white"
+          className="underline text-gray-500"
         >
-          GET AN INJURY-SPECIFIC RECOVERY PROGRAM HERE
+          terms and conditions.
         </a>
-      </Button>
+        </p>
+
+      </div>
     </div>
   );
 });
