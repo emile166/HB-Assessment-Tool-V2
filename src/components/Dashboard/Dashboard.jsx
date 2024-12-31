04fc117bd91f7de9f2d1090a7b4919f7ed79c4b0
emile166 <emilemodesitt@gmail.com>
2024-12-31 10:27:10 -0800

    Dashboard rules update.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -41,8 +41,8 @@ function Dashboard({ onSelectQuestionnaire }) {
           <Card className="p-8 mb-12 bg-gray-100">
             <h2 className="text-xl font-bold text-red-500 mb-2 text-center underline">Rules:</h2>
             <ol className="list-decimal list-inside text-md">
-              <li>Assess one injury at a time.</li>
-              <li>Read ALL answers before selecting one (some questions allow multiple answers).</li>
+              <li>Assess one finger injury at a time.</li>
+              <li>Read ALL answers before submitting (some have some but crucial differences). Some questions allow multiple answers.</li>
               <li>Only select answers that are relevant to the injury you’re assessing. (If a test causes completely unrelated symptoms, don't put that in your answers.)</li>
               <li>Start with the primary assessment unless otherwise instructed.</li>
               <li>Complete all applicable assessments in the one sitting (typically 2-3 assessments requiring 20-40 minutes in total).</li>
